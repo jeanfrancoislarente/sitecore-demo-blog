d0c08ca364bf3df405edded685afe12307397893
Scott Mulligan <scm@sitecore.net>
2021-01-27 13:25:11 -0500

    removing reference to alert component

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,6 @@ export default function Layout({ preview, children }) {
     <>
       <Meta />
       <div className="min-h-screen">
-        <Alert preview={preview} />
         <main>{children}</main>
       </div>
       <Footer />
