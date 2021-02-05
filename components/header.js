fa1318be620d497c30d8c26aec7201af7ad35ed3
Jean-François Larente <jfl@sitecore.net>
2021-02-05 08:31:56 -0500

    Initial commit of logo and new header

--- components/header.js
+++ components/header.js
@@ -9,7 +9,7 @@ export default function Header() {
         <nav className="navbar navbar-expand-sm mb-10">
           <div className="container-fluid pe-0 ps-0">
             <Link className="" href="/">
-              <a className="text-2xl md:text-4xl lg:text-6xl xl:text-7xl whitespace-nowrap font-bold tracking-tight md:tracking-tighter leading-tight mt-2 mb-2 hover:underline">{BLOG_NAME}.</a>
+              <a className="text-2xl md:text-4xl lg:text-6xl xl:text-7xl whitespace-nowrap font-bold tracking-tight md:tracking-tighter leading-tight mt-2 mb-2 hover:underline"><img src="../assets/blog/shared/Sitecore-logo-RGB-1728x374.png" style={{width:300}}/> {BLOG_NAME}.</a>
             </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
