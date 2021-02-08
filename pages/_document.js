1534647e353ecd41e2e1c59f40f09c4d3742635a
Jean-François Larente <jfl@sitecore.net>
2021-02-08 07:09:48 -0500

    Refactoring gtag

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,31 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-QKTJM6MS1W" >
-          </script>
-          <script dangerouslySetInnerHTML={
-              { __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){window.dataLayer.push(arguments)}
-                  gtag("js", new Date());
-                  gtag("config", "G-QKTJM6MS1W");
-              `}
-            }>
-          </script>
+        <Fragment>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </Fragment>
         </Head>
         <body>
           <Main />
