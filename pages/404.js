3ab02595fbdee37693a682fe70774b3661dbb534
topazahmed <thisistopaz@hotmail.com>
2021-02-09 03:07:04 -0700

    added error page and favicon (#16)
    * 404 page added
    * favicon added, using the same one from sitecore.com hotlinked
    * minor fix and formatting of other files
    
    Co-authored-by: Minhaz Topaz <mint@sitecore.net>
    Co-authored-by: Jean-François Larente <jfl@sitecore.net>

---
+++ pages/404.js
@@ -0,0 +1,35 @@
+import Container from '../components/container'
+import Layout from '../components/layout'
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Four0four() {
+  return (
+    <Layout>
+      <Head>
+        <title>Sitecore Demo Blog - This is not the page you are looking for...</title>
+      </Head>
+      <Container>
+        <h1>Uh Oh!</h1>
+        <h3>We know this is not the page you want to see. </h3>
+        <p>
+          We could not find the page you are looking for. It could have:
+        </p>
+        <ul>
+          <li>moved to another place</li>
+          <li>renamed to a better name</li>
+          <li>been deemed useless and have been deleted, or</li>
+          <li>never existed in the first place</li>
+        </ul>
+        <p>
+          <Link href="/">
+            <a>
+              Please click here and go back to the home page and read all the other useful blogs we have to offer.
+            </a>
+          </Link>
+        </p>
+      </Container>
+    </Layout>
+  )
+}
+
