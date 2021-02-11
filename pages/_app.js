82ef375fd856317b4e4149c50c1dc0795e80e201
Jean-François Larente <jfl@sitecore.net>
2021-02-11 11:16:06 -0500

    Trying to set Google analytics only in production environment

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,11 @@ import Router from 'next/router'
 
 import * as gtag from '../lib/gtag'
 
-// Notice how we track pageview when route is changed
-Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
+if (process.env.VERCEL_ENV === 'production') {
+  // Notice how we track pageview when route is changed
+  Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
+}
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
