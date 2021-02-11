add76ded1d14ee5e55283fc0d1e1d3a40a73f4ba
J.F. Larente <5884676+jeanfrancoislarente@users.noreply.github.com>
2021-02-11 12:26:33 -0500

    Trying to set Google analytics only in production environment (#27)

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,12 @@ import Router from 'next/router'
 
 import * as gtag from '../lib/gtag'
 
-// Notice how we track pageview when route is changed
-Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
+const isProduction = process.env.VERCEL_ENV === 'production';
 
+if (isProduction) {
+  // Notice how we track pageview when route is changed
+  Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
+}
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
