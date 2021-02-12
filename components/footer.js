e4cbebe2bb8681f8dddde9780477be1075e9ec61
Jean-François Larente <jfl@sitecore.net>
2021-02-12 06:52:49 -0500

    Added real urls

--- components/footer.js
+++ components/footer.js
@@ -5,8 +5,32 @@ import { BLOG_NAME } from '../lib/constants'
 export default function Footer() {
   return (
     <footer>
-      <div className="flex-col md:flex-row flex items-center md:justify-between site-footer mt-10">
+      <div className="flex-col flex  site-footer mt-10">
         <Container>
+          <div className="container-fluid pe-0 ps-0">
+            <div className="row pt-8 pb-8">
+              <div className="col">
+                <Link href="/">
+                  <a href="https://www.sitecore.com" target="_blank" className="mt-2 mb-2">Sitecore</a>
+                </Link>
+              </div>
+              <div className="col">
+                <Link href="/">
+                  <a href="https://doc.sitecore.com/" target="_blank" className="mt-2 mb-2">Sitecore Documentation</a>
+                </Link>
+              </div>
+              <div className="col">
+                <Link href="/">
+                  <a href="https://www.sitecore.com/request-a-demo" target="_blank" className="mt-2 mb-2">Request a Demo</a>
+                </Link>
+              </div>
+              <div className="col">
+                <Link href="/">
+                  <a href="https://www.youtube.com/user/mastersitecore" target="_blank" className="mt-2 mb-2">Discover Sitecore</a>
+                </Link>
+              </div>
+            </div>
+          </div>
         </Container>
       </div>
     </footer>
