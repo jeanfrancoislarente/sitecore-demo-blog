66db3aa24e8c387c131892ed493c1f8e3872f394
Neli Kostadinova <nko@sitecore.net>
2023-10-24 16:16:41 +0300

    Make image clickable

--- components/AuthorPreview.tsx
+++ components/AuthorPreview.tsx
@@ -14,13 +14,15 @@ export default function AuthorPreview({ name, photo, jobTitle, bio, slug }: Auth
   return (
     <div className="flex flex-col items-start gap-4 mb-12 md:flex-row">
       {photo && (
-        <Image
-          src={photo.fileUrl}
-          width={100}
-          height={100}
-          className="rounded-full mr-4 w-28 h-28 object-cover shrink-0"
-          alt={photo.name}
-        />
+        <Link href={`/team/${slug}`}>
+          <Image
+            src={photo.fileUrl}
+            width={100}
+            height={100}
+            className="rounded-full mr-4 w-28 h-28 object-cover shrink-0"
+            alt={photo.name}
+          />
+        </Link>
       )}
       <div className="w-full">
         <h2 className="text-3xl leading-snug">
