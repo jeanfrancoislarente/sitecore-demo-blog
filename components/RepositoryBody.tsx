9a106f12190fdb856905233badd76bc4c1a698bc
Neli Kostadinova <nko@sitecore.net>
2023-10-27 15:56:36 +0300

    Initial setup

--- components/RepositoryBody.tsx
+++ components/RepositoryBody.tsx
@@ -1,11 +1,7 @@
-import markdownStyles from './markdown-styles.module.css';
-
 type RepositoryBodyProps = {
   content: string;
 };
 
 export default function RepositoryBody({ content }: RepositoryBodyProps) {
-  return (
-    <div className={markdownStyles['markdown']} dangerouslySetInnerHTML={{ __html: content }} />
-  );
+  return <div dangerouslySetInnerHTML={{ __html: content }} />;
 }
