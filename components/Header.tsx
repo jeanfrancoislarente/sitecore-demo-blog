a5d5b4686459348ff5c125bd9ce091762b3cd3f6
Neli Kostadinova <nko@sitecore.net>
2023-10-31 11:52:39 +0200

    Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,59 @@
-import Container from '../markdown/components/container';
 import Link from 'next/link';
-import { BLOG_NAME } from '../markdown/lib/constants';
+import { BLOG_NAME } from '../lib/constants';
 import Image from 'next/image';
+import { faBars, faClose, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Header() {
+  const router = useRouter();
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <header>
-      <Container>
-        <nav>
-          <div>
-            <Link href="/">
-              <Image
-                src="/assets/blog/shared/site-title-web.png"
-                width={560}
-                height={68}
-                alt={BLOG_NAME}
-              />
-            </Link>
+    <header className="header">
+      <nav>
+        <div className="container header-container">
+          <Link href="/" className="header-logo">
+            <Image
+              src="/assets/blog/shared/site-title-web.png"
+              width={330}
+              height={40}
+              alt={BLOG_NAME}
+            />
+          </Link>
+          <button className="header-button">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+          <button className="header-button" onClick={() => setMenuOpen(true)}>
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+        </div>
+        <div className={`header-menu ${menuOpen ? 'visible' : ''}`}>
+          <div className="container header-menu-container">
+            <button className="header-button">
+              <FontAwesomeIcon icon={faClose} onClick={() => setMenuOpen(false)} />
+            </button>
             <ul>
-              <li>
-                <Link href={`/repositories`}>Our Repositories</Link>
+              <li className={router.pathname == '/' ? 'active' : ''}>
+                <Link href={`/`}>Blog</Link>
               </li>
-              <li>
-                <Link href={`/team`}>Our Team</Link>
+              <li className={router.pathname.startsWith('/products') ? 'active' : ''}>
+                <Link href={`/products`}>Products</Link>
+              </li>
+              <li className={router.pathname.startsWith('/repositories') ? 'active' : ''}>
+                <Link href={`/repositories`}>Repositories</Link>
+              </li>
+              <li className={router.pathname.startsWith('/team') ? 'active' : ''}>
+                <Link href={`/team`}>Team</Link>
+              </li>
+              <li className={router.pathname.startsWith('/contact') ? 'active' : ''}>
+                <Link href={`/contact`}>Contact</Link>
               </li>
             </ul>
           </div>
-        </nav>
-      </Container>
+        </div>
+      </nav>
     </header>
   );
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,2 @@
 export const BLOG_NAME = 'Sitecore Demo Team Blog.';
-export const HOME_OG_IMAGE_URL = '/assets/blog/welcome/lighthouse-main.png';
 export const IS_PRODUCTION = process.env.VERCEL_ENV === 'production';
