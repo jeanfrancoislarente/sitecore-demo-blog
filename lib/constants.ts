b4ba37beddef344b2bf53f757ed40917a297711c
Alexander Doroshenko <adoprog@users.noreply.github.com>
2023-11-21 10:20:07 -0500

    Meta updates

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,4 @@
 export const BLOG_NAME = 'Sitecore Demo Team Blog';
+export const BLOG_DESCRIPTION =
+  'Meet Sitecore Demo team — dynamic, talented, and committed! Explore blog posts about sales demo, deployment, open source, and other insightful content. Join us on our journey, sharing experiences, discussing issues, and enhancing the understanding of Sitecore products.';
 export const IS_PRODUCTION = process.env.VERCEL_ENV === 'production';
